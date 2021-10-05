5a958768a0ffd3d743583be3c92681e22155eadf
Deepshikha <deepshikha.mec@gmail.com>
2021-10-05 16:07:57 +0530

    feat(Routing): Added routing to the application

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
+import {BrowserRouter as Router , Route} from 'react-router-dom';
+import Home from './components/Home/Home';
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router className="App">
+      <Route path='/' exact component= {Home} />
+
+    </Router> 
   );
 }
 
